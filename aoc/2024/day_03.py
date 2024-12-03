0a6ab33a751c8379a1e5bc5159d71d9914a0a59d
Jason Cameron <git@jasoncameron.dev>
2024-12-03 01:30:37 -0500

    Add day 3

--- aoc/2024/day_03.py
+++ aoc/2024/day_03.py
@@ -1,24 +1,8 @@
-from collections import Counter, defaultdict, deque
+import re
 
 import aoc_helper
 from aoc_helper import (
-    Grid,
-    PrioQueue,
-    SparseGrid,
-    decode_text,
     extract_ints,
-    extract_iranges,
-    extract_ranges,
-    extract_uints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    multirange,
-    range,
-    search,
-    tail_call,
 )
 
 raw = aoc_helper.fetch(3, 2024)
@@ -30,12 +14,17 @@ def parse_raw(raw: str):
 
 data = parse_raw(raw)
 
-
 # providing this default is somewhat of a hack - there isn't any other way to
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_one(data=data):
-    ...
+    total = 0
+    results = re.findall(r'(mul\([0-9]{1,3},[0-9]{1,3}\))', data)
+    for result in results:
+        ints = extract_ints(result)
+        total += ints[0] * ints[1]
+    return total
+
 
 
 aoc_helper.lazy_test(day=3, year=2024, parse=parse_raw, solution=part_one)
@@ -45,7 +34,23 @@ aoc_helper.lazy_test(day=3, year=2024, parse=parse_raw, solution=part_one)
 # force type inference to happen, AFAIK - but this won't work with standard
 # collections (list, set, dict, tuple)
 def part_two(data=data):
-    ...
+    total = 0
+    results = re.findall(r"(mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\))", data)
+    ignore_result = False
+
+    for result in results:
+        if result == "don't()":
+            ignore_result = True
+        elif result == "do()":
+            ignore_result = False
+        if ignore_result:
+            continue
+        ints = extract_ints(result)
+        if not ints:
+            continue
+        total += ints[0] * ints[1]
+    return total
+
 
 
 aoc_helper.lazy_test(day=3, year=2024, parse=parse_raw, solution=part_two)
