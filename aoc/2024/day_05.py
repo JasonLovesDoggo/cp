54530f945e8107e42a9a2e10fe55501d6e1dcd1e
Jason Cameron <git@jasoncameron.dev>
2024-12-05 00:20:34 -0500

    add day 5

--- aoc/2024/day_05.py
+++ aoc/2024/day_05.py
@@ -1,52 +1,82 @@
-from collections import Counter, defaultdict, deque
+import functools
+from collections import defaultdict
 
 import aoc_helper
-from aoc_helper import (
-    Grid,
-    PrioQueue,
-    SparseGrid,
-    decode_text,
-    extract_ints,
-    extract_iranges,
-    extract_ranges,
-    extract_uints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    multirange,
-    range,
-    search,
-    tail_call,
-)
 
 raw = aoc_helper.fetch(5, 2024)
 
 
 def parse_raw(raw: str):
-    return raw
+    sections = raw.strip().split('\n\n')
+    rules = []
+    for line in sections[0].split('\n'):
+        before, after = map(int, line.split('|'))
+        rules.append((before, after))
+    updates = []
+    for line in sections[1].split('\n'):
+        updates.append(list(map(int, line.split(','))))
+    return rules, updates
 
+def is_valid_order(update, rules):
+    for x, y in rules:
+        if x in update and y in update:
+            x_index = update.index(x)
+            y_index = update.index(y)
+            if x_index > y_index:
+                return False
+    return True
 
-data = parse_raw(raw)
-
-
-# providing this default is somewhat of a hack - there isn't any other way to
-# force type inference to happen, AFAIK - but this won't work with standard
-# collections (list, set, dict, tuple)
-def part_one(data=data): ...
-
+def part_one(data=None):
+    rules, updates = data
+    middle_page_sum = 0
+    for update in updates:
+        if is_valid_order(update, rules):
+            middle_page = update[len(update) // 2]
+            middle_page_sum += middle_page
+    return middle_page_sum
 
 aoc_helper.lazy_test(day=5, year=2024, parse=parse_raw, solution=part_one)
+aoc_helper.lazy_submit(day=5, year=2024, solution=part_one, data=raw)
 
+def part_two(raw):
+    sections = raw.strip().split('\n\n')
+    rules = []
+    for line in sections[0].split('\n'):
+        before, after = map(int, line.split('|'))
+        rules.append((before, after))
+    updates = []
+    for line in sections[1].split('\n'):
+        updates.append(list(map(int, line.split(','))))
 
-# providing this default is somewhat of a hack - there isn't any other way to
-# force type inference to happen, AFAIK - but this won't work with standard
-# collections (list, set, dict, tuple)
-def part_two(data=data): ...
+    def is_valid_order(update):
+        for x, y in rules:
+            if x in update and y in update:
+                x_index = update.index(x)
+                y_index = update.index(y)
+                if x_index > y_index:
+                    return False
+        return True
 
+    def correct_order(update):
+        corrected = update.copy()
+        while True:
+            changed = False
+            for x, y in rules:
+                if x in corrected and y in corrected and corrected.index(x) > corrected.index(y):
+                    corrected.remove(x)
+                    corrected.insert(corrected.index(y), x)
+                    changed = True
+            if not changed:
+                break
+        return corrected
+
+    middle_page_sum = 0
+    for update in updates:
+        if not is_valid_order(update):
+            corrected_update = correct_order(update)
+            middle_page = corrected_update[len(corrected_update) // 2]
+            middle_page_sum += middle_page
+    return middle_page_sum
 
 aoc_helper.lazy_test(day=5, year=2024, parse=parse_raw, solution=part_two)
-
-aoc_helper.lazy_submit(day=5, year=2024, solution=part_one, data=data)
-aoc_helper.lazy_submit(day=5, year=2024, solution=part_two, data=data)
+aoc_helper.lazy_submit(day=5, year=2024, solution=part_two, data=raw)
