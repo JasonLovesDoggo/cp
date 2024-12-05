3d490b29083896e772c8641dc4b815c47c286809
Jason Cameron <git@jasoncameron.dev>
2024-12-05 00:21:01 -0500

    fmnt day 5

--- aoc/2024/day_05.py
+++ aoc/2024/day_05.py
@@ -1,21 +1,19 @@
-import functools
-from collections import defaultdict
-
 import aoc_helper
 
 raw = aoc_helper.fetch(5, 2024)
 
 
 def parse_raw(raw: str):
-    sections = raw.strip().split('\n\n')
+    sections = raw.strip().split("\n\n")
     rules = []
-    for line in sections[0].split('\n'):
-        before, after = map(int, line.split('|'))
+    for line in sections[0].split("\n"):
+        before, after = map(int, line.split("|"))
         rules.append((before, after))
     updates = []
-    for line in sections[1].split('\n'):
-        updates.append(list(map(int, line.split(','))))
+    for line in sections[1].split("\n"):
+        updates.append(list(map(int, line.split(","))))
     return rules, updates
+
 
 def is_valid_order(update, rules):
     for x, y in rules:
@@ -26,6 +24,7 @@ def is_valid_order(update, rules):
                 return False
     return True
 
+
 def part_one(data=None):
     rules, updates = data
     middle_page_sum = 0
@@ -35,18 +34,20 @@ def part_one(data=None):
             middle_page_sum += middle_page
     return middle_page_sum
 
+
 aoc_helper.lazy_test(day=5, year=2024, parse=parse_raw, solution=part_one)
 aoc_helper.lazy_submit(day=5, year=2024, solution=part_one, data=raw)
 
+
 def part_two(raw):
-    sections = raw.strip().split('\n\n')
+    sections = raw.strip().split("\n\n")
     rules = []
-    for line in sections[0].split('\n'):
-        before, after = map(int, line.split('|'))
+    for line in sections[0].split("\n"):
+        before, after = map(int, line.split("|"))
         rules.append((before, after))
     updates = []
-    for line in sections[1].split('\n'):
-        updates.append(list(map(int, line.split(','))))
+    for line in sections[1].split("\n"):
+        updates.append(list(map(int, line.split(","))))
 
     def is_valid_order(update):
         for x, y in rules:
@@ -62,7 +63,11 @@ def part_two(raw):
         while True:
             changed = False
             for x, y in rules:
-                if x in corrected and y in corrected and corrected.index(x) > corrected.index(y):
+                if (
+                    x in corrected
+                    and y in corrected
+                    and corrected.index(x) > corrected.index(y)
+                ):
                     corrected.remove(x)
                     corrected.insert(corrected.index(y), x)
                     changed = True
@@ -77,6 +82,7 @@ def part_two(raw):
             middle_page = corrected_update[len(corrected_update) // 2]
             middle_page_sum += middle_page
     return middle_page_sum
+
 
 aoc_helper.lazy_test(day=5, year=2024, parse=parse_raw, solution=part_two)
 aoc_helper.lazy_submit(day=5, year=2024, solution=part_two, data=raw)
